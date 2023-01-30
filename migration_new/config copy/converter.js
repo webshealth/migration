22e730fe6526513736b529addbd8c36dbb2dd665
chanakaudithasandaruwan <121252932+webshealth@users.noreply.github.com>
2023-01-31 00:49:42 +0530

    add strapi

--- migration_new/config copy/converter.js
+++ migration_new/config copy/converter.js
@@ -11,26 +11,28 @@ module.exports = async () => {
   const data = JSON.parse(result);
   const titles_and_content_dates = [];
 
-  data.data.post.map((i) => {
-    
-    // if (i.Title._text !== undefined) {
-    //   titles_and_dates.push({
-    //     name: i.Title._text,
-    //     updateDate: i.PostModifiedDate._text,
-    //   });
-    // }
-  });
+  console.log(data);
 
-  fs.writeFile(
-    "titles_and_dates.txt",
-    JSON.stringify(titles_and_dates),
-    function (err) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log("The file was saved!");
-    }
-  );
+  // const outPut = data.data.post.filter((i) => {
+  //   if (
+  //     i.Title._text ===
+  //     "Top Blockchain Conferences 2022: Where to get industry insight?"
+  //   ) {
+  //     return true;
+  //   }
+  // });
+  // console.log(outPut);
+
+  // fs.writeFile(
+  //   "titles_and_dates.txt",
+  //   JSON.stringify(titles_and_dates),
+  //   function (err) {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+  //     console.log("The file was saved!");
+  //   }
+  // );
 
   // await Promise.all(
   //   data.rss.channel.item.map((item) => {
