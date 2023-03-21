57e23eaa5a81dca0ebf30eb8eab51dad136fc655
webshealth <webshealthmail@gmail.com>
2023-03-21 17:50:38 +0530

    Revert "Do many changes"
    This reverts commit 1b536baf81f8377a503bf21fb10e31f9b4943090.

---
+++ strapi-practice/config/converter.js
@@ -0,0 +1,42 @@
+"use-strict";
+
+module.exports = async () => {
+  var convert = require("xml-js");
+  const fs = require("fs");
+  var xml = require("fs").readFileSync("./Posts-Export-2023-January-26-0527.xml", "utf8");
+
+  // var list = [];
+
+  var result = convert.xml2json(xml, { compact: true, spaces: 4 });
+  const data = JSON.parse(result);
+  const titles_and_content_dates = [];
+  // console.log(data.data.post[0])
+
+  const outPut = data.data.post.filter((i) => {
+    if (
+      i.Title._text ===
+      "New Bitfinex Flash Crashes Prove Devastating for Traders: What Happened?"
+    ) {
+      return true;
+    }
+  });
+  console.log(outPut[0].Content._cdata);
+
+  // fs.writeFile(
+  //   "titles_and_dates.txt",
+  //   JSON.stringify(titles_and_dates),
+  //   function (err) {
+  //     if (err) {
+  //       return console.log(err);
+  //     }
+  //     console.log("The file was saved!");
+  //   }
+  // );
+
+  // await Promise.all(
+  //   data.rss.channel.item.map((item) => {
+  //     // console.log(item["wp:postmeta"]);
+  //     a.push({ titles: "title1", contenet: "content1" });
+  //   })
+  // );
+};
